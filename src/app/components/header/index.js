5f711e7553006bb52ff02e4ad0f79c16dfe749fa
Mateus <mateusfpmelodev@gmail.com>
2024-07-30 11:36:50 -0300

    checkpoint

---
+++ src/app/components/header/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import styles from './header.module.css'
+
+const HeaderModule = () => {
+
+
+    return(
+        <>
+            <div className="firstcontent">
+                <div className={styles.header}>
+                    <h1>Lorem Ipsum Lorem</h1>
+                    <p className="p18">33 asperiores asperiores non molestiae voluptas aut molestias reiciendis et dicta fugit. Est totam sint et omnis repellat At adipisci blanditiis eum numquam cumque? Est velit impedit eos nihil sequi et nihil iusto.</p>
+                    <button className="bt1">Lorem Ipsum</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+HeaderModule.displayName = 'HeaderModule'
+export default HeaderModule
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
 import Image from "next/image"
 import styles from "./page.module.css"
 import Navbar from "./components/navbar"
+import HeaderModule from "./components/header"
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
-      <div className={styles.description}>
-        Inicio
-      </div>
+      <HeaderModule/>
     </main>
   )
 }
